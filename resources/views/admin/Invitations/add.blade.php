9cb4455d9297dad281d4b8b6dbc08d0cd8eefc87
Hamad Bakeel <hamadbakeel@gmail.com>
2023-08-13 18:21:05 +0300

    updated invitation add form

--- resources/views/admin/Invitations/add.blade.php
+++ resources/views/admin/Invitations/add.blade.php
@@ -63,16 +63,6 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <select class="form-select form-select py-4" aria-label="Select example">
-                        <option>التصنيف</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
                     <div class="form-floating mb-5 mb-md-0">
                         <input type="file" class="form-control" id="customFile">
                         <label for="customFile">
@@ -80,179 +70,30 @@
                         </label>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row">
-                        <span class="fs-3">تفعيل تعدد النماذج</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input" type="radio" value="yes" id="flexRadioDefault" name="multiVersion" />
-                            <label class="form-check-label fs-4 me-2 ms-6" for="flexRadioDefault">
-                                لا
-                            </label>
-
-                            <label class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="radio" value="no" name="multiVersion" />
-                                <span class="form-check-label fs-4 me-2">
-                                    نعم
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
+                    <select class="form-select form-select py-4" aria-label="Select example">
+                        <option>الخط</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div class="col-md-6">
                     <div class="form-floating mb-5 mb-md-0">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                        <input type="color" class="form-control" id="floatingInput" placeholder="name@example.com" />
                         <label for="floatingInput">
-                            الترتيب
+                            لون الخط
                         </label>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <select class="form-select form-select py-4" aria-label="Select example">
-                        <option>عدد الحقول في كل صف</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row mb-5 mb-md-0">
-                        <span class="fs-3">دمج الصور في الخلفية</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input" type="radio" value="yes" id="flexRadioDefault" name="multiVersion" />
-                            <label class="form-check-label fs-4 me-2 ms-6" for="flexRadioDefault">
-                                لا
-                            </label>
 
-                            <label class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="radio" value="no" name="multiVersion" />
-                                <span class="form-check-label fs-4 me-2">
-                                    نعم
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row">
-                        <span class="fs-3 ">صيغة تحميل البطاقة</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input ms-2" type="checkbox" value="1" id="flexCheckDefault" />
-                            <label class="form-check-label" for="flexCheckDefault">
-                                بي دي اف
-                            </label>
-                        </div>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input ms-2" type="checkbox" value="1" id="flexCheckDefault" />
-                            <label class="form-check-label" for="flexCheckDefault">
-                                صورة
-                            </label>
-                        </div>
-                    </div>
-                </div>
             </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <select class="form-select form-select py-4 mb-5 mb-md-0" aria-label="Select example">
-                        <option>الإتجاه</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-                <div class="col-md-6">
-                    <select class="form-select form-select py-4" aria-label="Select example">
-                        <option>الحجم</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row mb-5 mb-md-0">
-                        <span class="fs-3">تفعيل الأرشفة في محرات البحث</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input" type="radio" value="yes" id="flexRadioDefault" name="multiVersion" />
-                            <label class="form-check-label fs-4 me-2 ms-6" for="flexRadioDefault">
-                                لا
-                            </label>
 
-                            <label class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="radio" value="no" name="multiVersion" />
-                                <span class="form-check-label fs-4 me-2">
-                                    نعم
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row">
-                        <span class="fs-3">تشفير رابط البطاقة</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input" type="radio" value="yes" id="flexRadioDefault" name="multiVersion" />
-                            <label class="form-check-label fs-4 me-2 ms-6" for="flexRadioDefault">
-                                لا
-                            </label>
-
-                            <label class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="radio" value="no" name="multiVersion" />
-                                <span class="form-check-label fs-4 me-2">
-                                    نعم
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row mb-5 mb-md-0">
-                        <span class="fs-3">تشفير رابط البطاقة</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input" type="radio" value="yes" id="flexRadioDefault" name="multiVersion" />
-                            <label class="form-check-label fs-4 me-2 ms-6" for="flexRadioDefault">
-                                مفعلة
-                            </label>
-
-                            <label class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="radio" value="no" name="multiVersion" />
-                                <span class="form-check-label fs-4 me-2">
-                                    معطلة
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-control d-flex align-items-center gap-4 flex-row mb-5 mb-md-0">
-                        <span class="fs-3">تخصيص تصميم الصفحة</span>
-                        <div class="form-check form-check-custom form-check-solid">
-                            <input class="form-check-input" type="radio" value="yes" id="flexRadioDefault" name="multiVersion" />
-                            <label class="form-check-label fs-4 me-2 ms-6" for="flexRadioDefault">
-                                لا
-                            </label>
-
-                            <label class="form-check form-check-custom form-check-solid">
-                                <input class="form-check-input" type="radio" value="no" name="multiVersion" />
-                                <span class="form-check-label fs-4 me-2">
-                                    نعم
-                                </span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div>
-
                 <button type="submit" class="btn btn-success">تأكيد</button>
                 <button class="btn btn-danger">رجوع</button>
-
             </div>
         </form>
         <!--end::Form container-->
